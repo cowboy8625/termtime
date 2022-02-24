144243db26bad0cabe0c8976f9d92a10cc3828d7
cowboy8625 <cowboy8625@protonmail.com>
2022-02-24 11:05:07 -0600

    Removed set list of fonts.
    All fonts for figlet are loaded on start of termtime.

--- src/main.rs
+++ src/main.rs
@@ -13,49 +13,6 @@ use rand::{thread_rng, Rng};
 use std::io::{Stdout, Write};
 use std::time::{Duration, Instant};
 
-// TODO: Remove list and get list from figet font folder.
-const FONTS: [&str; 39] = [
-    "ascii9",
-    "ascii12",
-    "banner",
-    "big",
-    "bigascii9",
-    "bigascii12",
-    "bigmono9",
-    "bigmono12",
-    "block",
-    "bubble",
-    "circle",
-    "digital",
-    "emboss",
-    "emboss2",
-    "future",
-    "ivrit",
-    "lean",
-    "letter",
-    "mini",
-    "mnemonic",
-    "mono9",
-    "mono12",
-    "pagga",
-    "script",
-    "shadow",
-    "slant",
-    "small",
-    "smascii9",
-    "smascii12",
-    "smblock",
-    "smbraille",
-    "smmono9",
-    "smmono12",
-    "smscript",
-    "smshadow",
-    "smslant",
-    "standard",
-    "term",
-    "wideterm",
-];
-
 impl StrTuple<(u64, u64)> for String {
     fn into_tuple(self) -> (u64, u64) {
         let mut nums = Vec::new();
@@ -110,7 +67,7 @@ trait StrTuple<T> {
 struct Flags {
     message: String,
     algin_message: Alignment,
-    font: usize,
+    font: String,
     fg: Option<(u8, u8, u8)>,
     bg: Option<(u8, u8, u8)>,
 }
@@ -133,7 +90,7 @@ impl Flags {
     }
 }
 
-fn args() -> Flags {
+fn args() -> Result<Flags, Box<dyn std::error::Error>> {
     let matches = Command::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
@@ -149,7 +106,7 @@ fn args() -> Flags {
             Arg::new("font")
                 .short('f')
                 .long("font")
-                .help(&*format!("Set font name {:#?}", FONTS))
+                .help(&*format!("Set font name font name."))
                 .takes_value(true),
         )
         .arg(
@@ -190,20 +147,17 @@ WARNING over rides -f flag",
                 .takes_value(true),
         )
         .get_matches();
-    let font = matches.value_of("font").unwrap_or("").to_string();
 
     let random = matches.is_present("random");
     let font = if random {
-        thread_rng().gen_range(0..FONTS.len())
+        let fonts = font_list()?;
+        let idx = thread_rng().gen_range(0..fonts.len());
+        fonts[idx].to_string()
     } else {
-        FONTS
-            .iter()
-            .enumerate()
-            .find(|(_, i)| i == &&font)
-            .map(|(i, _)| i)
-            .unwrap_or(0)
+        matches.value_of("font").unwrap_or("slant").to_owned()
     };
-    Flags {
+
+    Ok(Flags {
         message: matches.value_of("message").unwrap_or("").to_string(),
         algin_message: match matches.value_of("algin_message").unwrap_or("") {
             "center" => Alignment::Center,
@@ -213,7 +167,7 @@ WARNING over rides -f flag",
         font,
         fg: matches.value_of("foreground").map(StrTuple::into_tuple),
         bg: matches.value_of("background").map(StrTuple::into_tuple),
-    }
+    })
 }
 
 fn figet_message(
@@ -223,7 +177,7 @@ fn figet_message(
 ) -> Result<String, Box<dyn std::error::Error>> {
     Ok(String::from_utf8(
         std::process::Command::new("figlet")
-            .args(&["-f", FONTS[flags.font]])
+            .args(&["-f", flags.font.as_str()])
             .args(&["-w", width.to_string().as_str()])
             // .arg("-t")
             .arg("-c")
@@ -247,12 +201,12 @@ fn rpad(msg: &str) -> String {
         .collect()
 }
 
-fn remove_empty_lines(msg: &str) -> String {
-    msg.lines()
-        .filter(|l| !l.chars().all(|c| c == ' '))
-        .map(|s| format!("{}\n", s))
-        .collect::<String>()
-}
+// fn remove_empty_lines(msg: &str) -> String {
+//     msg.lines()
+//         .filter(|l| !l.chars().all(|c| c == ' '))
+//         .map(|s| format!("{}\n", s))
+//         .collect::<String>()
+// }
 
 fn printer(
     stdout: &mut Stdout,
@@ -261,7 +215,7 @@ fn printer(
     flags: &Flags,
     align: Alignment,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let message = rpad(&remove_empty_lines(message));
+    let message = rpad(message);
     let text_height = message.lines().count() as u16;
     let y = match align {
         Alignment::Top => 0,
@@ -306,7 +260,7 @@ fn events_system(
     width: &mut u16,
     height: &mut u16,
     running: &mut bool,
-    flags: &mut Flags,
+    //flags: &mut Flags,
 ) -> Result<(), Box<dyn std::error::Error>> {
     if event::poll(Duration::from_secs(1))? {
         let events = event::read()?;
@@ -318,18 +272,20 @@ fn events_system(
                 } => {
                     *running = false;
                 }
-                KeyEvent {
-                    code: KeyCode::Up,
-                    modifiers: KeyModifiers::NONE,
-                } => {
-                    flags.font = (flags.font + 1) % FONTS.len();
-                }
-                KeyEvent {
-                    code: KeyCode::Down,
-                    modifiers: KeyModifiers::NONE,
-                } => {
-                    flags.font = (flags.font - 1) % FONTS.len();
-                }
+                // KeyEvent {
+                //     code: KeyCode::Up,
+                //     modifiers: KeyModifiers::NONE,
+                // } => {
+                //     flags.font = (flags.font + 1) % font_len;
+                //     clear(stdout)?;
+                // }
+                // KeyEvent {
+                //     code: KeyCode::Down,
+                //     modifiers: KeyModifiers::NONE,
+                // } => {
+                //     flags.font = (flags.font - 1) % fonts_len;
+                //     clear(stdout)?;
+                // }
                 _ => {}
             },
             Event::Resize(w, h) => {
@@ -343,8 +299,26 @@ fn events_system(
     Ok(())
 }
 
+fn font_list() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    use std::ffi::OsStr;
+    let paths = std::fs::read_dir("/usr/share/figlet/")?;
+
+    let mut font_names = Vec::new();
+    for dir_entry in paths {
+        let path = dir_entry?.path();
+        if path.extension() == Some(&OsStr::new("tlf")) {
+            match path.file_stem() {
+                Some(name) => font_names.push(name.to_string_lossy().to_string()),
+                None => {}
+            }
+        }
+    }
+    Ok(font_names)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut flags = args();
+    //let font_len = font_list()?.len();
+    let flags = args()?;
     enable_raw_mode()?;
     let mut stdout = std::io::stdout();
     let (mut width, mut height) = terminal::size()?;
@@ -358,7 +332,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &mut width,
             &mut height,
             &mut running,
-            &mut flags,
+            //    &mut flags,
         )?;
         display(&mut stdout, width, height, start, &flags)?;
     }
